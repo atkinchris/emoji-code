798d71c7e9b492c2167ed2498877052974c48fcd
Chris Atkin <atkinchris@gmail.com>
2018-09-28 09:08:36 +0100

    Add emoji saving

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { saveSvgAsPng } from 'save-svg-as-png'
 
-const Canvas = ({ components }) => (
-  <div className="emoji with-border">
-    <svg width="100%" height="100%" viewBox="0 0 100 100">
-      {components}
-    </svg>
-  </div>
-)
+const Canvas = ({ components }) => {
+  const svgElement = React.createRef()
+  const saveSvg = () => {
+    saveSvgAsPng(svgElement.current, `emoji_${new Date().toISOString()}.png`, { scale: 4 })
+  }
+
+  return (
+    <div className="flex-card flex-card--align-center">
+      <div className="emoji with-border">
+        <svg width="100%" height="100%" viewBox="0 0 100 100" ref={svgElement}>
+          {components}
+        </svg>
+      </div>
+      <button type="button" className="button with-border" onClick={saveSvg}>
+        Save Emoji
+      </button>
+    </div>
+  )
+}
 
 Canvas.propTypes = {
   components: PropTypes.arrayOf(PropTypes.node).isRequired,
