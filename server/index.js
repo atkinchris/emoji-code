c27113fabc88bc6c9156bb1791b64c27d9c57fa9
dani quinn sexton <dani.sexton@sainsburys.co.uk>
2018-10-03 11:58:44 +0100

    Give emojis unique names when saved on the server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const multer = require('multer')
+const uniquefilename = require('uniquefilename')
+const path = require('path')
 
 const app = express()
 const port = 8080
 
+const EMOJI_DIRECTORY = 'emoji/'
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'emoji/')
+    cb(null, EMOJI_DIRECTORY)
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
+    const fullUploadPath = path.resolve(EMOJI_DIRECTORY, file.originalname)
+
+    uniquefilename.get(fullUploadPath, {}, uniqueFileName => {
+      cb(null, path.basename(uniqueFileName))
+    })
   },
 })
 
@@ -17,25 +25,15 @@ const upload = multer({
   storage,
 })
 
-app.post(
-  '/uploadji',
-  upload.single('emoji'),
-  (req, res) => {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-    console.log(req.file)
-    res.sendStatus(200)
-  }
-)
+app.post('/uploadji', upload.single('emoji'), (req, res) => {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.file)
+  res.sendStatus(200)
+})
 
-app.get(
-  '/',
-  (req, res) => res.send('helloji 🌚'),
-)
+app.get('/', (req, res) => res.send('helloji 🌚'))
 
-app.listen(
-  port,
-  () => {
-    console.log(`Emoji Collector listening on port ${port}!`)
-  }
-)
+app.listen(port, () => {
+  console.log(`Emoji Collector listening on port ${port}!`)
+})
