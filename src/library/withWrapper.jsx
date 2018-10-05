e93fbde1c7192c597dcaf42dd4b10c2906320405
Chris Atkin <atkinchris@gmail.com>
2018-10-05 10:43:31 +0100

    Fix rotation origins for parts

--- src/library/withWrapper.jsx
+++ src/library/withWrapper.jsx
@@ -4,9 +4,14 @@ const fromPercentage = percentage => parseInt(percentage, 10) / 100
 
 const withWrapper = Component => {
   const wrapper = (x = 50, y = 50, scale = 100, rotate = 0) => (
-    <g transform={`translate(${x}, ${y}) rotate(${rotate})`} style={{ transformOrigin: 'center' }}>
+    <g transform={`translate(${x}, ${y})`} style={{ transformOrigin: 'center' }}>
       <svg x="-50" y="-50">
-        <g style={{ transform: `scale(${fromPercentage(scale)})`, transformOrigin: 'center' }}>
+        <g
+          style={{
+            transform: `rotate(${rotate}deg) scale(${fromPercentage(scale)})`,
+            transformOrigin: 'center',
+          }}
+        >
           <Component />
         </g>
       </svg>
