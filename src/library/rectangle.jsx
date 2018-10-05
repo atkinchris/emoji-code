2f88dddc46eb22d5ecc87abe666387ecb86d59ba
dani quinn sexton <helloitsdan@users.noreply.github.com>
2018-10-05 16:56:49 +0100

    Add categories emoji to clarify library a bit (#16)

--- src/library/rectangle.jsx
+++ src/library/rectangle.jsx
@@ -7,5 +7,6 @@ const Rectangle = (x = 0, y = 0, width = 50, height = 25, colour = '#f46767') =>
 Rectangle.args = ['x', 'y', 'width', 'height', 'colour']
 
 Rectangle.previewViewBox = '0 0 50 25'
+Rectangle.category = "Shapes"
 
 export default Rectangle
