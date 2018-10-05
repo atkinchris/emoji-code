77e907308c02d728288b7264d488401ec52b61c5
Chris Atkin <atkinchris@gmail.com>
2018-10-05 10:23:29 +0100

    Import to library

--- src/library/index.js
+++ src/library/index.js
@@ -1,14 +1,62 @@
+import bearEar from './svgs/bearEar.svg'
+import bearMouth from './svgs/bearMouth.svg'
+import catEar from './svgs/catEar.svg'
+import catEyeLeft from './svgs/catEyeLeft.svg'
+import catEyeRight from './svgs/catEyeRight.svg'
+import catMouth from './svgs/catMouth.svg'
+import chipmunk from './svgs/chipmunk.svg'
 import circle from './svgs/circle.svg'
+import cross from './svgs/cross.svg'
+import dollar from './svgs/dollar.svg'
+import eggplant from './svgs/eggplant.svg'
 import eye from './svgs/eye.svg'
+import eyeCalm from './svgs/eyeCalm.svg'
+import eyeCheeky from './svgs/eyeCheeky.svg'
+import eyeCryingLeft from './svgs/eyeCryingLeft.svg'
+import eyeCryingRight from './svgs/eyeCryingRight.svg'
+import eyeLaughingLeft from './svgs/eyeLaughingLeft.svg'
+import eyeLaughingRight from './svgs/eyeLaughingRight.svg'
+import eyeOpen from './svgs/eyeOpen.svg'
+import eyeRolling from './svgs/eyeRolling.svg'
+import eyeSparkle from './svgs/eyeSparkle.svg'
+import eyeThinking from './svgs/eyeThinking.svg'
 import eyeWinking from './svgs/eyeWinking.svg'
-import mouthBlowing from './svgs/mouthBlowing.svg'
-import mouthGrinning from './svgs/mouthGrinning.svg'
+import glasses from './svgs/glasses.svg'
+import handHugging from './svgs/handHugging.svg'
+import handHuggingLeft from './svgs/handHuggingLeft.svg'
+import handHuggingRight from './svgs/handHuggingRight.svg'
+import handThinking from './svgs/handThinking.svg'
 import heart from './svgs/heart.svg'
-import sparkle from './svgs/sparkle.svg'
+import huff from './svgs/huff.svg'
+import hundred from './svgs/hundred.svg'
+import moonDark from './svgs/moonDark.svg'
+import moonLight from './svgs/moonLight.svg'
 import mouth from './svgs/mouth.svg'
+import mouthBlowing from './svgs/mouthBlowing.svg'
+import mouthConfused from './svgs/mouthConfused.svg'
+import mouthDrool from './svgs/mouthDrool.svg'
+import mouthFrowning from './svgs/mouthFrowning.svg'
+import mouthGrinning from './svgs/mouthGrinning.svg'
+import mouthHalfSmile from './svgs/mouthHalfSmile.svg'
 import mouthLaughing from './svgs/mouthLaughing.svg'
+import mouthMoney from './svgs/mouthMoney.svg'
+import mouthNeutral from './svgs/mouthNeutral.svg'
+import mouthShocked from './svgs/mouthShocked.svg'
+import mouthSick from './svgs/mouthSick.svg'
+import mouthSmiling from './svgs/mouthSmiling.svg'
+import mouthTongue from './svgs/mouthTongue.svg'
+import mouthYawning from './svgs/mouthYawning.svg'
 import poop from './svgs/poop.svg'
+import pug from './svgs/pug.svg'
+import pumpkin from './svgs/pumpkin.svg'
+import snout from './svgs/snout.svg'
+import sparkle from './svgs/sparkle.svg'
+import sunglasses from './svgs/sunglasses.svg'
+import tada from './svgs/tada.svg'
 import tear from './svgs/tear.svg'
+import unicorn from './svgs/unicorn.svg'
+import z from './svgs/z.svg'
+import zipper from './svgs/zipper.svg'
 
 import unicornHorn from './unicornHorn'
 import rectangle from './rectangle'
@@ -17,18 +65,66 @@ import semicircle from './semicircle'
 import withWrapper from './withWrapper'
 
 export default {
-  circle: withWrapper(circle),
-  eye: withWrapper(eye),
-  eyeWinking: withWrapper(eyeWinking),
-  heart: withWrapper(heart),
-  mouth: withWrapper(mouth),
-  mouthBlowing: withWrapper(mouthBlowing),
-  mouthGrinning: withWrapper(mouthGrinning),
-  mouthLaughing: withWrapper(mouthLaughing),
-  poop: withWrapper(poop),
   rectangle,
   semicircle,
-  sparkle: withWrapper(sparkle),
-  tear: withWrapper(tear),
   unicornHorn,
+  bearEar: withWrapper(bearEar),
+  bearMouth: withWrapper(bearMouth),
+  catEar: withWrapper(catEar),
+  catEyeLeft: withWrapper(catEyeLeft),
+  catEyeRight: withWrapper(catEyeRight),
+  catMouth: withWrapper(catMouth),
+  chipmunk: withWrapper(chipmunk),
+  circle: withWrapper(circle),
+  cross: withWrapper(cross),
+  dollar: withWrapper(dollar),
+  eggplant: withWrapper(eggplant),
+  eye: withWrapper(eye),
+  eyeCalm: withWrapper(eyeCalm),
+  eyeCheeky: withWrapper(eyeCheeky),
+  eyeCryingLeft: withWrapper(eyeCryingLeft),
+  eyeCryingRight: withWrapper(eyeCryingRight),
+  eyeLaughingLeft: withWrapper(eyeLaughingLeft),
+  eyeLaughingRight: withWrapper(eyeLaughingRight),
+  eyeOpen: withWrapper(eyeOpen),
+  eyeRolling: withWrapper(eyeRolling),
+  eyeSparkle: withWrapper(eyeSparkle),
+  eyeThinking: withWrapper(eyeThinking),
+  eyeWinking: withWrapper(eyeWinking),
+  glasses: withWrapper(glasses),
+  handHugging: withWrapper(handHugging),
+  handHuggingLeft: withWrapper(handHuggingLeft),
+  handHuggingRight: withWrapper(handHuggingRight),
+  handThinking: withWrapper(handThinking),
+  heart: withWrapper(heart),
+  huff: withWrapper(huff),
+  hundred: withWrapper(hundred),
+  moonDark: withWrapper(moonDark),
+  moonLight: withWrapper(moonLight),
+  mouth: withWrapper(mouth),
+  mouthBlowing: withWrapper(mouthBlowing),
+  mouthConfused: withWrapper(mouthConfused),
+  mouthDrool: withWrapper(mouthDrool),
+  mouthFrowning: withWrapper(mouthFrowning),
+  mouthGrinning: withWrapper(mouthGrinning),
+  mouthHalfSmile: withWrapper(mouthHalfSmile),
+  mouthLaughing: withWrapper(mouthLaughing),
+  mouthMoney: withWrapper(mouthMoney),
+  mouthNeutral: withWrapper(mouthNeutral),
+  mouthShocked: withWrapper(mouthShocked),
+  mouthSick: withWrapper(mouthSick),
+  mouthSmiling: withWrapper(mouthSmiling),
+  mouthTongue: withWrapper(mouthTongue),
+  mouthYawning: withWrapper(mouthYawning),
+  poop: withWrapper(poop),
+  pug: withWrapper(pug),
+  pumpkin: withWrapper(pumpkin),
+  snout: withWrapper(snout),
+  sparkle: withWrapper(sparkle),
+  sunglasses: withWrapper(sunglasses),
+  tada: withWrapper(tada),
+  tear: withWrapper(tear),
+  unicorn: withWrapper(unicorn),
+  z: withWrapper(z),
+  zipper: withWrapper(zipper),
 }
